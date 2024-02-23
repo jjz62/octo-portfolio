3a070f1d4c3d70279a7d586926ecfe343ad0ec53
Jenny Zhang <jjz62@cornell.edu>
2024-02-23 10:03:09 -0500

    breakpoint adjustments

--- site.js
+++ site.js
@@ -1,4 +1,9 @@
-$("#arrow").click(function(){
-    $("#landing-screen").slideDown();
-  });
-  
+$(window).on('resize', function() {
+  if($(window).width() > 864) {
+      $('#introduction').addClass('row');
+      $('#introduction').removeClass('col-8');
+  }else{
+      $('#profile-image').addClass('row');
+      $('#profile-image').removeClass('col-4');
+  }
+})
